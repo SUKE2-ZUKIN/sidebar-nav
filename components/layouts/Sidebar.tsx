6fbf6c65ac7d3ff976bf08a44fd483371cce8f9a
SUKE2-ZUKIN <mono.316b@gmail.com>
2022-12-14 06:36:01 +0900

    GH-1: WIP) add sidebar compornents

---
+++ components/layouts/Sidebar.tsx
@@ -0,0 +1,14 @@
+import { SidebarDetail } from "./SidebarDetail";
+
+const Sidebar = () => {
+  return (
+    <div className="Sidebar">
+      <ul className="SidebarList">
+        
+        <SidebarDetail />
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
---
+++ components/layouts/SidebarDetail.tsx
@@ -0,0 +1,9 @@
+import HomeIcon from '@mui/icons-material/Home';
+
+export const SidebarDetail = [
+  {
+    title: "ホーム",
+    icon: <HomeIcon />,
+    link: "/home"
+  }
+];
