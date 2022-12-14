c14b9473ce402737a258da44e48ba74e0d57257a
SUKE2-ZUKIN <mono.316b@gmail.com>
2022-12-15 06:43:50 +0900

    GH-1: WIP) fix sidebar

--- components/layouts/Sidebar.tsx
+++ components/layouts/Sidebar.tsx
@@ -4,8 +4,16 @@ const Sidebar = () => {
   return (
     <div className="Sidebar">
       <ul className="SidebarList">
-        
-        <SidebarDetail />
+        {SidebarDetail.map((value, key) => {
+          return (
+            <li key={key} className="row" onClick={() => {
+              window.location.pathname = value.link; 
+            }}>
+              <div id="icon">{value.icon}</div>
+              <div id="icon">{value.title}</div>
+            </li>
+          )
+        })}
       </ul>
     </div>
   );
--- components/layouts/SidebarDetail.tsx
+++ components/layouts/SidebarDetail.tsx
@@ -1,9 +1,51 @@
 import HomeIcon from '@mui/icons-material/Home';
+import EmailIcon from '@mui/icons-material/Email';
+import AssessmentIcon from '@mui/icons-material/Assessment';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import PaymentIcon from '@mui/icons-material/Payment';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import SettingsIcon from '@mui/icons-material/Settings';
 
-export const SidebarDetail = [
+interface DetailType {
+  title: string;
+  icon: any;
+  link: string;
+}
+
+export const SidebarDetail: DetailType[] = [
   {
     title: "ホーム",
     icon: <HomeIcon />,
-    link: "/home"
-  }
+    link: "/home",
+  },
+  {
+    title: "メール",
+    icon: <EmailIcon />,
+    link: "/mail",
+  },
+  {
+    title: "アナリティクス",
+    icon: <AssessmentIcon />,
+    link: "/analytics",
+  },
+  {
+    title: "友達追加",
+    icon: <PersonAddIcon />,
+    link: "/friends",
+  },
+  {
+    title: "お支払い設定",
+    icon: <PaymentIcon />,
+    link: "/payment",
+  },
+  {
+    title: "アップロード",
+    icon: <CloudUploadIcon />,
+    link: "/upload",
+  },
+  {
+    title: "詳細設定",
+    icon: <SettingsIcon />,
+    link: "/details",
+  },
 ];
