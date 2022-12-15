0de18f60f9f94981bd0f9ffe3703da857986fbb7
SUKE2-ZUKIN <mono.316b@gmail.com>
2022-12-16 06:44:08 +0900

    GH-1: WIP) fix sidebar

--- components/layouts/Sidebar.tsx
+++ components/layouts/Sidebar.tsx
@@ -1,16 +1,31 @@
+import { useEffect, useState } from "react";
 import { SidebarDetail } from "./SidebarDetail";
+import { SidebarIcon } from "./SidebarIcon";
 
 const Sidebar = () => {
+  const [path, setPath] = useState<string>("");
+  
+  useEffect(() => {
+    setPath(window.location.pathname);
+    console.log(path);
+  }, [path]);
+  
   return (
     <div className="Sidebar">
+      <SidebarIcon />
       <ul className="SidebarList">
         {SidebarDetail.map((value, key) => {
           return (
-            <li key={key} className="row" onClick={() => {
-              window.location.pathname = value.link; 
-            }}>
+            <li 
+              key={key} 
+              id={path == value.link ? "active" : ""}
+              className="row" 
+              onClick={() => {
+                window.location.pathname = value.link;
+              }}
+            >
               <div id="icon">{value.icon}</div>
-              <div id="icon">{value.title}</div>
+              <div id="title">{value.title}</div>
             </li>
           )
         })}
---
+++ components/layouts/SidebarIcon.tsx
@@ -0,0 +1,12 @@
+import Icon from "../src/images/main-pc2.JPG";
+
+function SidebarIcon() {
+  return (
+    <div className="SidebarIcon">
+      <img src={Icon} alt="" />
+      <p>test@gmail.com</p>
+    </div>
+  );
+}
+
+export default SidebarIcon;
